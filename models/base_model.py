03aa762e05f3d03e8142bf8e3dbd915f914c8e38
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2024-02-11 11:57:47 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,22 +5,38 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Base class for all other classes in the project."""
+    """
+    Base class for all other classes in the project.
+    """
 
     def __init__(self):
-        """Constructor for Base class."""
+        """
+        Constructor for Base class.
+        """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns a string representation of the object."""
+        """
+        Returns a string representation of the object.
+        """
 
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
 
         self.updated_at = datetime.now()
 
